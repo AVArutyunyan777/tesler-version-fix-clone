52d0390060a9ed63b84522e47cb2566350a3ad81
Hrach Ghazaryan <hrach.ghazaryan.web@mail.com>
2022-11-09 18:57:48 +0400

    chhhh

--- src/index.tsx
+++ src/index.tsx
@@ -1,21 +1,16 @@
-// import React from "react";
-// import { render } from "react-dom";
-// import App from "./App";
-//
-// render(<App />, document.body);
-
 // import React from 'react';
-// import ReactDOM from 'react-dom';
-// import Preview from './App';
+// import ReactDOM from 'react-dom/client';
+// import App from './App';
 //
 // const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement);
+//
 // root.render(
 //     <React.StrictMode>
-//         <Preview />
-//     </React.StrictMode>
+//         <App />
+//     </React.StrictMode>,
 // );
-//
-// ReactDOM.render(<Preview />, document.getElementById('root'));
+
+
 
 import Preview from './App';
 
